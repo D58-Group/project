d836bea2eac58cdf048cc53a574c64b864857ee6
SanyaA1222 <sanyaarora1222@gmail.com>
2025-11-21 16:58:53 -0800

    mininet setup

---
+++ network.py
@@ -0,0 +1,20 @@
+from mininet.topo import Topo
+
+class SnifferTopo(Topo):
+    def build(self):
+        s1 = self.addSwitch('s1')
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        sn = self.addHost('sn')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(sn, s1)
+
+topos = {'sniffer': SnifferTopo}
+
+#run with: 
+#sudo mn --custom sniffer_topo.py --topo sniffer --mac --switch ovsk --controller none
+
+#to run our sniffer: 
+#mininet> h3 python3 sniffer.py
