3bda77c9b9597996e51aa0dec29c4dac2a33c604
MadisonMajarais <madison.majarais@gmail.com>
2025-11-29 17:23:46 -0800

    Update packet node struct

--- packet_sniffer.h
+++ packet_sniffer.h
@@ -15,7 +15,10 @@ struct packet_node {
 
   uint32_t src_ip;             
   uint32_t dst_ip;
-  uint8_t  proto;              
+  enum protocol proto;  
+  
+  unsigned char ar_sha[ETHER_ADDR_LEN];
+  unsigned char ar_tha[ETHER_ADDR_LEN];
 
   uint32_t length;             
   
